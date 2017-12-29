d59e171cfc5c56255b7b8815210a0242a4d4f587
Alim <alimhassam@gmail.com>
2017-12-29 08:23:59 -0500

    Improve mmol support.
     - Use bc to support floating point calculation, and other small improvements

--- xdrip-get-entries.sh
+++ xdrip-get-entries.sh
@@ -33,25 +33,24 @@ cat ./entry.json
 glucose=$(cat ./entry.json | jq -M $glucoseType)
 echo
 
-if [ "${glucose}" == "" ] ; then
+if [ -z "${glucose}" ] ; then
   echo "Invalid response from g5 transmitter"
   ls -al ./entry.json
   cat ./entry.json
   rm ./entry.json
 else
 
-  dg=`expr $glucose - $lastGlucose`
+  dg=$(bc -l <<< "$glucose - $lastGlucose")
 
   # begin try out averaging last two entries ...
   da=${dg}
-  if [ ${da} -lt 0 ]; then
-    da=`expr 0 - $da`
+  if [ -n ${da} -a $(bc <<< "${da} < 0") -eq 1 ]; then
+    da=$(bc <<< "0 - $da")
   fi
-  if [ ${da} -lt 45 -a ${da} -gt 6 ]; then
+  if (( $(bc <<< "(${da} < 45) && (${da} > 6)") )); then
      echo "Before Average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=${glucose}"
-     glucose=`expr $glucose + $lastGlucose`
-     glucose=`expr $glucose / 2`
-     dg=`expr $glucose - $lastGlucose`
+     glucose=$(bc -l <<< "($glucose + $lastGlucose)/2")
+     dg=$(bc -l <<< "$glucose - $lastGlucose")
      echo "After Average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=${glucose}"
   fi
   # end average last two entries if noise  code
@@ -83,7 +82,7 @@ else
   # TBD: meter BG from pumphistory doesn't support mmol yet - has no units...
   echo "meterbg from pumphistory: $meterbg" 
 
-  if [ "$meterbg" == "" ]; then
+  if [ -z $meterbg ]; then
     # look for a bg check from NS (& test NS record for local time or UTC)
     curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -u -d "7 minutes ago" -Iminutes)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null >> $METERBG_NS_RAW
     isUTC=$(jq ".[0].created_at[-1:]" $METERBG_NS_RAW)
@@ -95,29 +94,25 @@ else
       fi
     fi
 
-    meterbgunits=$(cat $METERBG_NS_RAW | jq -M '.[0] | .units')
-    meterbg=$(cat $METERBG_NS_RAW | jq -M '.[0] | .glucose')
-    meterbg="${meterbg%\"}"
-    meterbg="${meterbg#\"}"
+    meterbgunits=$(cat $METERBG_NS_RAW | jq -M '.[0] | .units' | tr -d '"')
+    meterbg=$(cat $METERBG_NS_RAW | jq -M '.[0] | .glucose' | tr -d '"')
     if [ "$meterbgunits" == "mmol" ]; then
-      meterbg=$(echo $meterbg "*18" | bc)
+      meterbg=$(bc -l <<< "$meterbg *18")
     fi
     echo "meterbg from nightscout: $meterbg" 
   fi
 
-  if [ "$meterbg" == "null" ]; then
-    :
-  else
-    if [ $meterbg -lt 400 -a $meterbg -gt 40 ]; then
+  if [ -n "$meterbg" ]; then
+    if (( bc -l <<< "($meterbg < 400) && ($meterbg > 40)" )); then
       calibrationBg=$meterbg
-      if [ "$lastPostCal" != "" ]; then
-        if [ $lastPostCal -lt 400 -a $lastPostCal -gt 40 ]; then
+      if [ -n "$lastPostCal" ]; then
+        if (( bc <<< "($lastPostCal < 400) && ($lastPostCal > 40)" )); then
           calibrationBg=$((($meterbg + $lastPostCal) / 2))
         fi
       fi
-      calibration=`expr $calibrationBg - $glucose`
+      calibration="$(bc -l <<< "$calibrationBg - $glucose")"
       echo "calibration=$calibration, meterbg=$meterbg, lastPostCal=$lastPostCal, calibrationBg=$calibrationBg, glucose=$glucose"
-      if [ $calibration -lt 60 -a $calibration -gt -80 ]; then
+      if (( bc -l <<< "($calibration < 60) && ($calibration > -80)" )); then
         # another safety check, but this is a good calibration
         echo "[{\"calibration\":${calibration}}]" > $CALIBRATION_STORAGE
         cp $METERBG_NS_RAW meterbg-ns-backup.json
@@ -127,7 +122,7 @@ else
 
   if [ -e $CALIBRATION_STORAGE ]; then
     calibration=$(cat $CALIBRATION_STORAGE | jq -M '.[0] | .calibration')
-    calibratedglucose=`expr $glucose + $calibration`
+    calibratedglucose=$(bc -l <<< "$glucose + $calibration")
     echo "After calibration calibratedglucose =$calibratedglucose"
   else
     echo "No valid calibration yet - exiting"
@@ -150,19 +145,19 @@ else
   direction='NONE'
   echo "Valid response from g5 transmitter"
 
-  if [ ${dg} -lt -10 ]; then
+  if (( $(bc <<< "${dg} < -10") )); then
      direction='DoubleDown'
-  elif [ ${dg} -lt -7 ]; then 
+  elif (( $(bc <<< "${dg} < -7") )); then
      direction='SingleDown'
-  elif [ ${dg} -lt -3 ]; then
+  elif (( $(bc <<< "${dg} < -3") )); then
      direction='FortyFiveDown'
-  elif [ ${dg} -lt 3 ]; then
+  elif (( $(bc <<< "${dg} < 3") )); then
      direction='Flat'
-  elif [ ${dg} -lt 7 ]; then
+  elif (( $(bc <<< "${dg} < 7") )); then
      direction='FortyFiveUp'
-  elif [ ${dg} -lt 10 ]; then
+  elif (( $(bc <<< "${dg} < 10") )); then
      direction='SingleUp'
-  elif [ ${dg} -lt 50 ]; then
+  elif (( $(bc <<< "${dg} < 50") )); then
      direction='DoubleUp'
   fi
 
