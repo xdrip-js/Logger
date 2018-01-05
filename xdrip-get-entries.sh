160693c9f92706e0aa54edfa88a4c4ba1fe025d6
fidoman <26698840+efidoman@users.noreply.github.com>
2018-01-05 11:40:22 -0500

    resolve UTC issue

--- xdrip-get-entries.sh
+++ xdrip-get-entries.sh
@@ -90,7 +90,12 @@ else
     createdAt=$(jq ".[0].created_at" $METERBG_NS_RAW)
 
     if [[ $createdAt != *"Z"* ]]; then
-      curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
+      if [ "$createdAt" != "NULL" -a "$createdAt" != "" ]; then
+        curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
+        echo "meterbg within 7 minutes using non-UTC time comparison."
+      fi
+    elif [[ $createdAt == *"Z" ]];then
+        echo "meterbg within 7 minutes using UTC time comparison."       
     fi
     
     meterbgunits=$(cat $METERBG_NS_RAW | jq -M '.[0] | .units')
