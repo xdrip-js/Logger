bf32d160b4d237572a9a30ebe8af47e8a565df25
Eric Feibelman <ericfeibelman@gmail.com>
2018-02-06 12:17:58 -0500

    reworked to support slope and y-intercept more cleanly

--- xdrip-get-entries.sh
+++ xdrip-get-entries.sh
@@ -20,13 +20,10 @@ CAL_OUTPUT="./calibration-linear.json"
 #  echo "sensor change - removing calibration"
 #  rm $CALIBRATION_STORAGE
 #fi
-calSlope=999
-b=16906
-m=914
+glucoseType='.[0].unfiltered'
 
 if [ -e "./entry.json" ] ; then
-  lastGlucose=$(cat ./entry.json | jq -M $glucoseType)
-  lastGlucose=$(($lastGlucose / $calSlope))
+  lastGlucose=$(cat ./entry.json | jq -M '.[0].glucose')
   
 #  lastAfter=$(date -d "5 minutes ago" -Iminutes)
 #  lastPostStr="'.[0] | select(.dateString > \"$lastAfter\") | .glucose'"
@@ -47,14 +44,83 @@ DEBUG=smp,transmitter,bluetooth-manager timeout 360s node logger $transmitter
 echo
 echo "after xdrip-js bg record below ..."
 cat ./entry.json
+glucose=$(cat ./entry.json | jq -M '.[0].glucose')
 
-# re-scale unfiltered from /1000 to /$calSlope
+if [ -z "${glucose}" ] ; then
+  echo "Invalid response from g5 transmitter"
+  ls -al ./entry.json
+  rm ./entry.json
+fi
 
-scaled=$(cat ./entry.json | jq -M $glucoseType)
-scaled=$(($scaled / $calSlope))
-tmp=$(mktemp)
-glucose=$scaled
-echo "scaled glucose=$glucose, scale=$calSlope"
+# capture raw values for use and for log to csv file 
+unfiltered=$(cat ./entry.json | jq -M '.[0].unfiltered')
+filtered=$(cat ./entry.json | jq -M '.[0].filtered')
+glucoseg5=$(cat ./entry.json | jq -M '.[0].glucose')
+datetime=$(date +"%Y-%m-%d %H:%M")
+
+# begin calibration logic - look for calibration from NS, use existing calibration or none
+ns_url="${NIGHTSCOUT_HOST}"
+METERBG_NS_RAW="meterbg_ns_raw.json"
+
+rm $METERBG_NS_RAW # clear any old meterbg curl responses
+
+# look for a bg check from pumphistory (direct from meter->openaps):
+meterbgafter=$(date -d "7 minutes ago" -Iminutes)
+meterjqstr="'.[] | select(._type == \"BGReceived\") | select(.timestamp > \"$meterbgafter\") | .amount'"
+meterbg=$(bash -c "jq $meterjqstr ~/myopenaps/monitor/pumphistory-merged.json")
+# TBD: meter BG from pumphistory doesn't support mmol yet - has no units...
+echo "meterbg from pumphistory: $meterbg"
+
+if [ -z $meterbg ]; then
+  curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes -u)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
+  createdAt=$(jq ".[0].created_at" $METERBG_NS_RAW)
+
+  if [[ $createdAt == *"Z"* ]]; then
+     echo "meterbg within 7 minutes using UTC time comparison."       
+  else   
+    if [ "$createdAt" != "null" -a "$createdAt" != "" ]; then
+      curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
+      echo "meterbg within 7 minutes using non-UTC time comparison."
+    fi
+  fi
+    
+  meterbgunits=$(cat $METERBG_NS_RAW | jq -M '.[0] | .units')
+  meterbg=`jq -M '.[0] .glucose' $METERBG_NS_RAW`
+  meterbg="${meterbg%\"}"
+  meterbg="${meterbg#\"}"
+  if [ "$meterbgunits" == "mmol" ]; then
+    meterbg=$(bc <<< "($meterbg *18)/1")
+  fi
+  echo "meterbg from nightscout: $meterbg"
+
+  if [ $(bc <<< "$meterbg < 400") -eq 1  -a $(bc <<< "$meterbg > 40") -eq 1 ]; then
+    echo "$unfiltered,$meterbg,$datetime" >> $CAL_INPUT
+    ./calc-calibration.sh $CAL_INPUT $CAL_OUTPUT
+  else
+    echo "Invalid calibration"
+  fi
+  cat $CAL_OUTPUT
+fi
+
+if [ -e $CAL_OUTPUT ]; then
+  slope=`jq -M '.[0] .slope' calibration-linear.json` 
+  yIntercept=`jq -M '.[0] .yIntercept' calibration-linear.json` 
+else
+  slope=1000
+  yIntercept=0
+fi
+
+calibratedBG=$(bc -l <<< "($unfiltered-$yIntercept)/$slope")
+calibratedBG=$(bc <<< "($calibratedBG/1)") # truncate
+echo "After calibration calibratedBG =$calibratedBG, slope=$slope, yIntercept=$yIntercept, unfiltered=$unfiltered"
+
+if [ $(bc <<< "$calibratedBG > 400") -eq 1 -o $(bc <<< "$calibratedBG < 40") -eq 1 ]; then
+  echo "Glucose $calibratedBG out of range [40,400] - exiting"
+  bt-device -r $id
+  exit
+fi
+
+glucose=$calibratedBG
 
 if [ -z "${glucose}" ] ; then
   echo "Invalid response from g5 transmitter"
@@ -74,100 +140,13 @@ else
     da=$(bc <<< "0 - $da")
   fi
   if [ "$da" -lt "45" -a "$da" -gt "15" ]; then
-     echo "Before Average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=$glucose"
-     glucose=$(bc <<< "($glucose + $lastGlucose)/2")
-     dg=$(bc <<< "$glucose - $lastGlucose")
-     echo "After average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=${glucose}"
+   echo "Before Average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=$glucose"
+   glucose=$(bc <<< "($glucose + $lastGlucose)/2")
+   dg=$(bc <<< "$glucose - $lastGlucose")
+   echo "After average last 2 entries - lastGlucose=$lastGlucose, dg=$dg, glucose=${glucose}"
   fi
   # end average last two entries if noise  code
 
-  # log to csv file for research g5 outputs
-  unfiltered=$(cat ./entry.json | jq -M '.[0].unfiltered')
-  filtered=$(cat ./entry.json | jq -M '.[0].filtered')
-  glucoseg5=$(cat ./entry.json | jq -M '.[0].glucose')
-  datetime=$(date +"%Y-%m-%d %H:%M")
-  # end log to csv file logic for g5 outputs
-
-  # begin calibration logic - look for calibration from NS, use existing calibration or none
-  calibration=0
-  ns_url="${NIGHTSCOUT_HOST}"
-  METERBG_NS_RAW="meterbg_ns_raw.json"
-
-  rm $METERBG_NS_RAW # clear any old meterbg curl responses
-
-  # look for a bg check from pumphistory (direct from meter->openaps):
-  meterbgafter=$(date -d "7 minutes ago" -Iminutes)
-  meterjqstr="'.[] | select(._type == \"BGReceived\") | select(.timestamp > \"$meterbgafter\") | .amount'"
-  meterbg=$(bash -c "jq $meterjqstr ~/myopenaps/monitor/pumphistory-merged.json")
-  # TBD: meter BG from pumphistory doesn't support mmol yet - has no units...
-  echo "meterbg from pumphistory: $meterbg"
-
-  if [ -z $meterbg ]; then
-    curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes -u)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
-    createdAt=$(jq ".[0].created_at" $METERBG_NS_RAW)
-
-    if [[ $createdAt == *"Z"* ]]; then
-       echo "meterbg within 7 minutes using UTC time comparison."       
-    else   
-      if [ "$createdAt" != "null" -a "$createdAt" != "" ]; then
-        curl -m 30 "${ns_url}/api/v1/treatments.json?find\[created_at\]\[\$gte\]=$(date -d "7 minutes ago" -Iminutes)&find\[eventType\]\[\$regex\]=Check" 2>/dev/null > $METERBG_NS_RAW
-        echo "meterbg within 7 minutes using non-UTC time comparison."
-      fi
-    fi
-    
-    meterbgunits=$(cat $METERBG_NS_RAW | jq -M '.[0] | .units')
-    meterbg=`jq -M '.[0] .glucose' $METERBG_NS_RAW`
-    meterbg="${meterbg%\"}"
-    meterbg="${meterbg#\"}"
-    if [ "$meterbgunits" == "mmol" ]; then
-      meterbg=$(bc <<< "($meterbg *18)/1")
-    fi
-    echo "meterbg from nightscout: $meterbg"
-
-if [ $(bc <<< "$meterbg < 400") -eq 1  -a $(bc <<< "$meterbg > 40") -eq 1 ]; then
-#      calibrationBg=$meterbg
-#      calibration="$(bc <<< "$calibrationBg - $glucose")"
-#      echo "calibration=$calibration, meterbg=$meterbg, calibrationBg=$calibrationBg, glucose=$glucose"
-#      if [ $(bc <<< "$calibration < 60") -eq 1 -a $(bc <<< "$calibration > -150") -eq 1 ]; then
-        # another safety check, but this is a good calibration
-#        if [ -e $CALIBRATION_STORAGE ]; then
-#          tmp=$(mktemp)
-#          jq ".[0].calibration = $calibration" $CALIBRATION_STORAGE > "$tmp" && mv "$tmp" $CALIBRATION_STORAGE 
-#        else
-#          echo "[{\"calibration\":$calibration}]" > $CALIBRATION_STORAGE
-#        fi
-        #Begin log calibrations for 1pt 2pt and regressive calibration calculations
-        echo "$unfiltered,$meterbg,$datetime" >> $CAL_INPUT
-        ./calc-calibration.sh $CAL_INPUT $CAL_OUTPUT
-
-#        cp $METERBG_NS_RAW meterbg-ns-backup.json
-      else
-        echo "Invalid calibration"
-      fi
-      cat $CAL_OUTPUT
-    fi
-  fi
-
-  if [ -e $CAL_OUTPUT ]; then
-#    calibration=$(cat $CALIBRATION_STORAGE | jq -M '.[0] | .calibration')
-    slope=`jq -M '.[0] .slope' calibration-linear.json` 
-    yIntercept=`jq -M '.[0] .yIntercept' calibration-linear.json` 
-  else
-    slope=1000
-    yIntercept=0
-#    echo "No valid calibration yet - exiting"
-#    bt-device -r $id
-#    exit
-  fi
-  calibratedglucose=$(bc <<< "$glucose + $calibration")
-  echo "After calibration calibratedglucose =$calibratedglucose"
-
-
-  if [ $(bc <<< "$calibratedglucose > 400") -eq 1 -o $(bc <<< "$calibratedglucose < 40") -eq 1 ]; then
-    echo "Glucose $calibratedglucose out of range [40,400] - exiting"
-    bt-device -r $id
-    exit
-  fi
 
   if [ $(bc <<< "$dg > 50") -eq 1 -o $(bc <<< "$dg < -50") -eq 1 ]; then
     echo "Change $dg out of range [50,-50] - exiting"
@@ -178,10 +157,10 @@ if [ $(bc <<< "$meterbg < 400") -eq 1  -a $(bc <<< "$meterbg > 40") -eq 1 ]; the
    cp entry.json entry-before-calibration.json
 
    tmp=$(mktemp)
-   jq ".[0].glucose = $calibratedglucose" entry.json > "$tmp" && mv "$tmp" entry.json
+   jq ".[0].glucose = $calibratedBG" entry.json > "$tmp" && mv "$tmp" entry.json
 
    tmp=$(mktemp)
-   jq ".[0].sgv = $calibratedglucose" entry.json > "$tmp" && mv "$tmp" entry.json
+   jq ".[0].sgv = $calibratedBG" entry.json > "$tmp" && mv "$tmp" entry.json
 
    tmp=$(mktemp)
    jq ".[0].device = \"${transmitter}\"" entry.json > "$tmp" && mv "$tmp" entry.json
@@ -235,13 +214,11 @@ if [ $(bc <<< "$meterbg < 400") -eq 1  -a $(bc <<< "$meterbg > 40") -eq 1 ]; the
 
 
   if [ ! -f "/var/log/openaps/g5.csv" ]; then
-    echo "datetime,unfiltered,filtered,glucoseg5,glucose,calibratedglucose,slope,direction,calibration,calibrationBg,predictedBg,slope,yIntercept" > /var/log/openaps/g5.csv
+    echo "datetime,unfiltered,filtered,glucoseg5,glucose,direction,calibratedBG,slope,yIntercept" > /var/log/openaps/g5.csv
   fi
 
 
-  predicted=$(bc -l <<< "($unfiltered-$yIntercept)/$slope")
-  predicted=$(bc <<< "($predicted/1)") # truncate
-  echo "${datetime},${unfiltered},${filtered},${glucoseg5},${glucose},${calibratedglucose},${calSlope},${direction},${calibration},${calibrationBg},${predicted},${slope},${yIntercept}" >> /var/log/openaps/g5.csv
+  echo "${datetime},${unfiltered},${filtered},${glucoseg5},${glucose},${direction},${calibratedBG},${slope},${yIntercept}" >> /var/log/openaps/g5.csv
 
   echo "Posting glucose record to xdripAPS"
   ./post-xdripAPS.sh ./entry-xdrip.json
