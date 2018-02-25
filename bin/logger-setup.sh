21b38012e8332746fe06180aa94d2b5377c28187
fidoman <ericfeibelman@gmail.com>
2018-02-25 18:54:33 -0500

    update setup

--- bin/logger-setup.sh
+++ bin/logger-setup.sh
@@ -2,7 +2,7 @@
 
 CALIBRATE="/usr/local/bin/calibrate"
 rm -f $CALIBRATE 
-ln -s ./bin/calibrate.sh $CALIBRATE
+ln -s /root/src/xdrip-js-logger/bin/calibrate.sh $CALIBRATE
 
 if type "go" > /dev/null; then
   echo "building calc-noise.go"
