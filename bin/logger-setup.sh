62731cad9616b38dbc926b40937f0980a8b10295
fidoman <ericfeibelman@gmail.com>
2018-03-01 12:57:41 -0500

    adding logger-noise script

--- bin/logger-setup.sh
+++ bin/logger-setup.sh
@@ -4,6 +4,10 @@ CALIBRATE="/usr/local/bin/calibrate"
 rm -f $CALIBRATE 
 ln -s /root/src/xdrip-js-logger/bin/calibrate.sh $CALIBRATE
 
+NOISE="/usr/local/bin/logger-noise"
+rm -f $NOISE 
+ln -s /root/src/xdrip-js-logger/noise.sh $NOISE
+
 if type "go" > /dev/null; then
   echo "building calc-noise.go"
   go build calc-noise.go
