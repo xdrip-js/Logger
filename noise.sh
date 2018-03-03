e8410e1f1e58da27cc0f35e1af74e911bc441844
fidoman <ericfeibelman@gmail.com>
2018-03-02 19:04:23 -0500

    added filtered/1k and unfiltered/1k values to logger-noise utility

--- noise.sh
+++ noise.sh
@@ -5,10 +5,11 @@ INPUT=${2:-"/var/log/openaps/g5.csv"}
 
 n=0
 
-echo "Datetime,      bg, noise, noise*100, noisesend"
 
 if [ -e $INPUT ]; then
   arrdate=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f1 ) )
+  unfiltered=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f3 ) )
+  filtered=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f4 ) )
   arrbg=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f6 ) )
   arrnoise=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f13 ) )
   arrnoisesend=( $(tail -$MAXRECORDS $INPUT | cut -d ',' -f14 ) )
@@ -19,11 +20,14 @@ fi
 for (( i=0; i<$n; i++ ))
 do
   arrdate[$i]=$(date -d @${arrdate[$i]} +%Y%m%d-%H:%M)
+  unfiltered[$i]=$(bc <<< "${unfiltered[$i]} / 1000")
+  filtered[$i]=$(bc <<< "${filtered[$i]} / 1000")
 done
 
+echo "Datetime, unfiltered/1k, filtered/1k, bg, noise, noise*100, noisesend"
 for (( i=0; i<$n; i++ ))
 do
 #  echo "${arrdate[$i]}"
   noisetimes100=$(bc <<< "${arrnoise[$i]} * 100")
-  echo "${arrdate[$i]}, ${arrbg[$i]}, ${arrnoise[$i]}, $noisetimes100, ${arrnoisesend[$i]}"
+  echo "${arrdate[$i]}, ${unfiltered[$i]}, ${filtered[$i]}, ${arrbg[$i]}, ${arrnoise[$i]}, $noisetimes100, ${arrnoisesend[$i]}"
 done
