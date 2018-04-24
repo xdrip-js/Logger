5a4686931799f1715f6f7a0f8e4167ca4437b272
fidoman <ericfeibelman@gmail.com>
2018-04-24 09:27:34 -0400

    updating to support non-expired tx

--- logger/index.js
+++ logger/index.js
@@ -1,8 +1,13 @@
 const Transmitter = require('xdrip-js');
 
 const id = process.argv[2];
-const transmitter = new Transmitter(id);
-
+// FIXME, process.argv[3] should probably just be a file containing an array of messages to send the transmitter instead of a json string.
+// examples mesages are: {date: Date.now(), type: "CalibrateSensor", glucose} or {date: Date.now(), type: "StopSensor"} or {date: Date.now(), type: "StartSensor"}
+const messages =  JSON.parse(process.argv[3] || '[]');
+console.log('messages to send: ' + JSON.stringify(messages));
+//messages.push({date: Date.now(), type: "CalibrateSensor", glucose})
+//const transmitter = new Transmitter(id); 
+const transmitter = new Transmitter(id, () => messages); 
 transmitter.on('glucose', glucose => {
   //console.log('got glucose: ' + glucose.glucose);
   lastGlucose = glucose;
@@ -12,7 +17,8 @@ transmitter.on('glucose', glucose => {
       'device': 'DexcomR4',
       'date': glucose.readDate,
       'dateString': new Date(glucose.readDate).toISOString(),
-      'sgv': Math.round(glucose.unfiltered/1000),
+      //'sgv': Math.round(glucose.unfiltered/1000),
+      'sgv': glucose.glucose,
       'direction': 'None',
       'type': 'sgv',
       'filtered': Math.round(glucose.filtered),
@@ -20,6 +26,7 @@ transmitter.on('glucose', glucose => {
       'rssi': "100", // TODO: consider reading this on connection and reporting
       'noise': "1",
       'trend': glucose.trend,
+      'state': glucose.state, // FIXME: make state a readable string
       'glucose': Math.round(glucose.glucose)
     }];
     const data = JSON.stringify(entry);
