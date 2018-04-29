8a223bf45ab9f385385229e26703c51b7a9d26c4
fidoman <ericfeibelman@gmail.com>
2018-04-29 07:57:52 -0400

    adding script to get current battery status or request a refresh. It already refreshes every 12 hours without manually specifying a refresh

---
+++ bin/g5-battery.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# optional parameter $1 to specify how many hours ago for sensor insert/start
+refresh=$1
+
+file="/root/src/xdrip-js-logger/g5-battery.json"
+if [ -n "$refresh" ]; then
+  echo "Queueing Battery Status Refresh message for next Tx transmission (5 to 10 minutes)"
+  touch -d "13 hours ago" $file
+else
+  cat $file
+  echo
+fi
