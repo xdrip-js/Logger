6c48d8b8ec68d0389c381210ca536480e9de549b
fidoman <ericfeibelman@gmail.com>
2018-05-05 05:43:02 -0400

    adding Logger script install to /usr/local/bin and cleanup setup script using function

--- bin/g5-reset.sh
+++ bin/g5-reset.sh
@@ -3,7 +3,8 @@
 MESSAGE="${HOME}/myopenaps/monitor/logger/g5-reset.json"
 epochdate=$(date +'%s%3N')
 
-echo "Running this command will instruct Logger to reset the g5 Transmitter!" 
+echo "Running this command will instruct Logger to reset the g5 Transmitter!"
+echo "   Your current session will be lost and will have to be restarted using g5-start" 
 
 read -p "Are you sure? (y/n)" -n 1 -r
 echo    
--- bin/logger-setup.sh
+++ bin/logger-setup.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-CALIBRATE="/usr/local/bin/calibrate"
-rm -f $CALIBRATE 
-ln -s /root/src/xdrip-js-logger/bin/calibrate.sh $CALIBRATE
-
-NOISE="/usr/local/bin/g5-noise"
-rm -f $NOISE 
-ln -s /root/src/xdrip-js-logger/bin/g5-noise.sh $NOISE
-
-STOP="/usr/local/bin/g5-stop"
-rm -f $STOP 
-ln -s /root/src/xdrip-js-logger/bin/g5-stop.sh $STOP
-
-START="/usr/local/bin/g5-start"
-rm -f $START 
-ln -s /root/src/xdrip-js-logger/bin/g5-start.sh $START
-
-BATTERY="/usr/local/bin/g5-battery"
-rm -f $BATTERY 
-ln -s /root/src/xdrip-js-logger/bin/g5-battery.sh $BATTERY
-
-RESET="/usr/local/bin/g5-reset"
-rm -f $RESET 
-ln -s /root/src/xdrip-js-logger/bin/g5-reset.sh $RESET
+function install_sh_bin()
+{
+  file=$1
+  bin="/usr/local/bin"
+  rm -f "${bin}/${file}"
+  ln -s ${HOME}/src/xdrip-js-logger/bin/${file}.sh ${bin}/${file}
+}
 
 mkdir -p ${HOME}/myopenaps/monitor/logger
 
+install_sh_bin "calibrate"
+install_sh_bin "g5-noise"
+install_sh_bin "g5-stop"
+install_sh_bin "g5-start"
+install_sh_bin "g5-battery"
+install_sh_bin "g5-reset"
+
+#CALIBRATE="/usr/local/bin/calibrate"
+#rm -f $CALIBRATE 
+#ln -s ${HOME}/src/xdrip-js-logger/bin/calibrate.sh $CALIBRATE
+
+LOGGER="/usr/local/bin/Logger"
+rm -f $LOGGER 
+ln -s ${HOME}/src/xdrip-js-logger/bin/xdrip-get-entries.sh $LOGGER
+
+
 if [ -e "/usr/local/go/bin/go" ]; then
   echo "building calc-noise.go"
+  cd ${HOME}/src/xdrip-js-logger
   go build calc-noise.go
   ls -al calc-noise
 fi
