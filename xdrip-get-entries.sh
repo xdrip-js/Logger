a8453350f984da7d06feff8f0de95033a8383193
fidoman <eric.feibelman@gmail.com>
2019-05-13 17:11:20 -0400

    reduce chance of missing a 5 minute window on slow hardware by only waiting 180 seconds vs 240 seconds between listens

--- xdrip-get-entries.sh
+++ xdrip-get-entries.sh
@@ -1449,8 +1449,8 @@ function check_last_glucose_time_smart_sleep()
     seconds_since_last_entry=$(bc <<< "$epochdate - $entry_timestamp")
     log "check_last_glucose_time - epochdate=$epochdate,  entry_timestamp=$entry_timestamp"
     log "Time since last glucose entry in seconds = $seconds_since_last_entry seconds"
-    sleep_time=$(bc <<< "240 - $seconds_since_last_entry") 
-    if [ $(bc <<< "$sleep_time > 0") -eq 1 -a $(bc <<< "$sleep_time < 240") -eq 1 ]; then
+    sleep_time=$(bc <<< "180 - $seconds_since_last_entry") 
+    if [ $(bc <<< "$sleep_time > 0") -eq 1 -a $(bc <<< "$sleep_time < 180") -eq 1 ]; then
       log "Waiting $sleep_time seconds because glucose records only happen every 5 minutes"
       wait_with_echo $sleep_time
     elif [ $(bc <<< "$sleep_time < -60") -eq 1 ]; then
