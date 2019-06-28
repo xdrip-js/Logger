b45ce58133957c798db80edeae66723a200c5a0a
fidoman <eric.feibelman@gmail.com>
2019-06-28 10:21:43 -0400

    further tweak to noise x-axis based on testing results

--- bin/g5-calc-noise.sh
+++ bin/g5-calc-noise.sh
@@ -47,7 +47,7 @@ fi
 firstDate=${xdate[0]}
 for (( i=0; i<$n; i++ ))
 do
-  xarr[$i]=$(bc -l <<< "(${xdate[$i]} - $firstDate) / 30") 
+  xarr[$i]=$(bc -l <<< "(${xdate[$i]} - $firstDate) / 20") 
 #  echo "x,y=${xarr[$i]},${yarr[$i]}"
 done
 
