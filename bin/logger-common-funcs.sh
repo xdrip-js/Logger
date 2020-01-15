f4ac0fa438ac13e7264b083d9c5082db31c6c9fd
fidoman <eric.feibelman@gmail.com>
2020-01-15 17:22:42 -0500

    do not time travel on cgm-stop for new firmware

--- bin/logger-common-funcs.sh
+++ bin/logger-common-funcs.sh
@@ -2,6 +2,8 @@
 #
 # Common functions for shell script components of Logger.
 
+LDIR="${HOME}/myopenaps/monitor/xdripjs"
+
 function newFirmware()
 {
   local version=$1
@@ -15,4 +17,15 @@ function newFirmware()
   esac
 }
 
+function txVersion()
+{
+  local tx_version=""
+  if [ -e "${LDIR}/tx-version.json" ]; then
+    tx_version=$(cat ${LDIR}/tx-version.json | jq -M '.firmwareVersion')
+    tx_version="${tx_version%\"}"
+    tx_version="${tx_version#\"}"
+  fi
+
+  echo $tx_version
+}
 
